b3034e95c7e4366052be18dcf1c39e3cb6e1778d
Roberto Marchetti <marchetti@beefree.io>
2025-05-17 13:34:37 +0200

    Better styles for react-select

--- src/styles/reactSelectStyles.ts
+++ src/styles/reactSelectStyles.ts
@@ -1,12 +1,23 @@
 import { StylesConfig } from 'react-select'
+interface StateConfig {
+  isOpen?: boolean
+  isFocused?: boolean
+  isSelected?: boolean
+}
+const ACCENT_COLOR = 'var(--accent-color)'
+
 export const reactSelectStyles = {
   container: (styles: StylesConfig['container']) => ({
     ...styles,
     width: '100%',
   }),
-  control: (styles: StylesConfig['control']) => ({
+  control: (styles: StylesConfig['control'], state: StateConfig) => ({
     ...styles,
     border: 'none',
+    borderColor: state.isFocused ? ACCENT_COLOR : styles.borderColor,
+    boxShadow: 'none',
+    outline: '4px solid var(--accent-color)',
+    outlineOffset: '-4px',
     borderRadius: '0',
     padding: '0.5rem 0.5rem',
     backgroundColor: 'transparent',
@@ -15,5 +26,39 @@ export const reactSelectStyles = {
   indicatorSeparator: (styles: StylesConfig['indicatorSeparator']) => ({
     ...styles,
     display: 'none',
+  }),
+  group: (styles: StylesConfig['group']) => ({
+    ...styles,
+    paddingBottom: '0',
+    marginBottom: '0',
+  }),
+  groupHeading: (styles: StylesConfig['groupHeading']) => ({
+    ...styles,
+    backgroundColor: '#eee',
+    margin: '0',
+    padding: '0.5rem 0.5rem',
+    fontWeight: 'bold',
+  }),
+  menu: (styles: StylesConfig['menu']) => ({
+    ...styles,
+    height: '100vh',
+    maxHeight: '100vh',
+    marginTop: '0',
+    borderRadius: '0',
+  }),
+  menuList: (styles: StylesConfig['menuList']) => ({
+    ...styles,
+    padding: '0',
+  }),
+  option: (styles: StylesConfig['option'], state: StateConfig) => ({
+    ...styles,
+    padding: '0.5rem 0.5rem',
+    backgroundColor: state.isSelected
+      ? ACCENT_COLOR
+      : state.isFocused
+        ? `${ACCENT_COLOR}` // Slightly transparent on hover
+        : undefined,
+    color: state.isSelected ? 'black' : 'inherit',
+    cursor: 'pointer',
   }),
 }
